928de41ee3c98731f2160dc37354a1160ca0fcf4
Tim.Sterker <tim.sterker@gleif.org>
2021-07-21 18:57:24 +0200

    test: Configure timeout

--- tests/config.php
+++ tests/config.php
@@ -3,3 +3,4 @@
 define('SOLR_HOST', getenv('TEST_SOLR_HOST') ? getenv('TEST_SOLR_HOST') : 'localhost');
 define('SOLR_PORT', getenv('TEST_SOLR_PORT') ? getenv('TEST_SOLR_PORT') : '8983');
 define('SOLR_PATH', getenv('TEST_SOLR_PATH') ? getenv('TEST_SOLR_PATH') : '/');
+define('SOLR_TIMEOUT', getenv('TEST_SOLR_TIMEOUT') ? getenv('TEST_SOLR_TIMEOUT') : '10');
